8fcb6a9732eec57e73af41cae15f6a349b4289a3
chidiebere onyedinma <codebreakersmd@gmail.com>
2020-02-27 18:19:50 -0500

    implemented remote controller

--- scripts/move.py
+++ scripts/move.py
@@ -97,22 +97,21 @@ def move(speed, direction, turn, radius=0.6):   # 0 < radius <= 1
     # speed = 100
     if direction == 'forward':
         if turn == 'left':
-            motor_left(0, left_backward, int(speed*radius))
-            print(int(speed*radius))
+            motor_left(1, left_backward, int(speed*radius))
             motor_right(1, right_forward, speed)
         elif turn == 'right':
             motor_left(1, left_forward, speed)
-            motor_right(0, right_backward, int(speed*radius))
+            motor_right(1, right_backward, int(speed*radius))
         else:
             motor_left(1, left_forward, speed)
             motor_right(1, right_forward, speed)
     elif direction == 'backward':
         if turn == 'left':
-            motor_left(0, left_forward, int(speed*radius))
+            motor_left(1, left_forward, int(speed*radius))
             motor_right(1, right_backward, speed)
         elif turn == 'right':
             motor_left(1, left_backward, speed)
-            motor_right(0, right_forward, int(speed*radius))
+            motor_right(1, right_forward, int(speed*radius))
         else:
             motor_left(1, left_backward, speed)
             motor_right(1, right_backward, speed)
